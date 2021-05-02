aef191fce41962bbb6b12c4d316c25ec70e1420c
hardiyan.purnama.hp@gmail.com <smart_m07>
2021-05-03 03:04:41 +0700

    mulai ngoding

---
+++ muka.py
@@ -0,0 +1,10 @@
+import cv2
+
+cam = cv2.VideoCapture(1)
+while True:
+    ret, frame = cam.read()
+    cv2.imshow('my face', frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+cam.release()
+cv2.destroyAllWindows()
