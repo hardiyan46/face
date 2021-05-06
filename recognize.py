0783b39e67c91de07505d3672777092e635b834e
hardiyan.purnama.hp@gmail.com <smart_m07>
2021-05-07 03:20:32 +0700

    recognize

--- recognize.py
+++ recognize.py
@@ -3,13 +3,13 @@ https://github.com/opencv/opencv/blob/master/data/haarcascades/haarcascade_front
 https://github.com/opencv/opencv/blob/master/data/haarcascades/haarcascade_eye.xml
 """
 import cv2, os
-wajahdir = assets
+wajahdir = 'assets'
 cam = cv2.VideoCapture(0)
 cam.set(3, 640)  # merubah lebar
 cam.set(4, 480)  # merubah tinggi
 facedetektor = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')  #baca algoritma face detektor
 eyedetektor = cv2.CascadeClassifier('haarcascade_eye.xml')   #baca algoritma eye detektor
-faceid = input("Ketikan Nama Lengkap Anda")
+faceid = input('Ketikan Nama Lengkap Anda : ')
 print ("Mohon tunggu proses pengambilan data wajah anda selesai...")
 ambildata = 1
 while True:
@@ -26,12 +26,13 @@ while True:
         eye = eyedetektor.detectMultiScale(roiabu)
         for (xe, ye, we, he) in eye:
             cv2.rectangle(roiwarna, (xe, ye), (xe + we, ye + he), (0, 0, 255), 1)
+
     cv2.imshow('my face', frame)  # menampilkan windows dengan nama frame
     # cv2.imshow('my face2', abu)   #tampilkan frame abu abu
     k = cv2.waitKey(1) & 0xFF  #Variabel keypress
     if k == 27 or k == ord('q'):  # key close dengan Q dan ESC
         break
-    elif ambildata>30:
+    elif ambildata>5:
         break
 print("pengambilan data wajah sudah selesai" )
 cam.release()
